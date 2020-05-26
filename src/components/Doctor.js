929175213a16c0ce2e53ee86c8fd40740220e1bb
creepysta <sweetnanispregnantghost@yahoo.com>
2020-05-26 07:38:59 +0530

    flex works for dr tile

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -3,18 +3,21 @@ import React from "react";
 const Doctor = ({doctor}) => {
 	return (
 		<div className="doctor-tile">
-			<div className="doctor-dp tile-dp">
-				<img src={doctor.avatar} alt={doctor.name}></img>
-			</div>
-			<div className="about-doctor">
-				{doctor.about}
+			<div className="top-row">
+				<div className="tile-dp">
+					<img src={doctor.avatar} alt={doctor.name} className="doctor-dp"></img>
+				</div>
+				<div className="about-doctor">
+				<p>Hi I am {doctor.name},</p>
+					{doctor.about}
+				</div>
+				<div className="appointment-btn">
+					<button className="btn tile-btn">Book Slot</button>
+				</div>
 			</div>
 			<hr className="about-qual-div"></hr>
 			<div className="doctor-qualifications">
 				{doctor.qualifications}
-			</div>
-			<div className="appointment-btn">
-				<button className="btn tile-btn">Book Slot</button>
 			</div>
 		</div>
 	)
