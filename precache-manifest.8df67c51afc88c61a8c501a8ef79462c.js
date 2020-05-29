572801faae0345ab84d449e9f6e8957a43aa6f85
creepysta <sweetnanispregnantghost@yahoo.com>
2020-05-29 12:07:09 +0530

    Updates

---
+++ precache-manifest.8df67c51afc88c61a8c501a8ef79462c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe368b80e04002bb29955039f066eba2",
+    "url": "/opd-management/index.html"
+  },
+  {
+    "revision": "ae11ecebb8494f5c8b50",
+    "url": "/opd-management/static/css/main.b8ae7ab7.chunk.css"
+  },
+  {
+    "revision": "043b264da66dc11452eb",
+    "url": "/opd-management/static/js/2.b1ac235a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/opd-management/static/js/2.b1ac235a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae11ecebb8494f5c8b50",
+    "url": "/opd-management/static/js/main.767d848d.chunk.js"
+  },
+  {
+    "revision": "561409f7cbffc628e1a6",
+    "url": "/opd-management/static/js/runtime-main.5f1b4c91.js"
+  }
+]);
